3ae25ec87b2281973cdb783c0c5fb4b627d63c35
ChenleiPei <130779454+ChenleiPei@users.noreply.github.com>
2025-02-09 20:15:52 +0100

    Update RMSE_for_our_method.py

--- RMSE_for_our_method.py
+++ RMSE_for_our_method.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def load_xy_values(xy_values_file):
     data = np.load(xy_values_file)
-    x_values = data[:, 0]  # 假设x值在每行的第一列
-    y_true = data[:, 1]    # 假设y值在每行的第二列
+    x_values = data[:, 0]  
+    y_true = data[:, 1]   
     return x_values, y_true
 
 def evaluate_equation(equation, x):
-    # 使用 eval 计算等式的值，这里假设等式是正确的 Python 表达式
+   
     return eval(equation)
 
 def calculate_rmse(equation, x_values, y_true):
@@ -15,11 +15,11 @@ def calculate_rmse(equation, x_values, y_true):
     rmse = np.sqrt(((y_pred - y_true) ** 2).mean())
     return rmse
 
-# 手动输入等式
+
 equation1 = "x*x*x + x*x + x"
 
 
-# 文件路径，需要根据实际情况进行修改
+
 xy_values_file = 'Koza_3_test.npy'
 
 x_values, y_true = load_xy_values(xy_values_file)
